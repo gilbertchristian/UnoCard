591ab7adb786e7b176aa5d1391a470cdcd8030b3
dimasmeisabraditya <dimasmeisabraditya@gmail.com>
2021-03-31 10:08:51 +0700

    kartu

---
+++ HijiCard.java
@@ -0,0 +1,40 @@
+package HijiCard;
+
+public class HijiCard{
+    enum Color{
+        Red, Blue, Green, Yellow, Wild, values;
+
+        private static final Color[] colors = Color.values();
+        public static Color getColor (int i){
+            return Color.colors[i];
+        }
+    }
+    enum Value{
+        Zero, One, Two, Trhee, Four, Five, Six, Seven, Eight, Nine, DrawTwoo, Skip, Reverse, Wild, Wild_Four;
+
+        private static final Value[] values = Value.values();
+        public static Value getValue (int i){
+            return Value.values[i];
+        }    
+    }
+    private final Color color;
+    private final Value value;
+
+    public HijiCard(final Color color, final Value value){
+        this.color = color;
+        this.value = value;
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+    public Value getValue(){
+        return this.value;
+    }
+    public String toString(){
+        return color + "_" + value;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
